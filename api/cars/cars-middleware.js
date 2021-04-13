415aa7bf41802828340e9d41473d7892db76c6b5
Liza Pincsak <lizapincsak@MacBook-Air-2.local>
2021-04-13 18:47:36 -0500

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,52 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Car = require("./cars-model");
+
+const checkCarId = async(req, res, next) => {
+  try{
+    const id = await Car.getById(req.params.id)
+    if(!id){
+      res.status(404).json({message: `car with id ${id} is not found`})
+    } else {
+      req.id = id
+      next()
+    }
+  }
+  catch(err){
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  if(!req.body.vin){
+    res.status(400).json({message: `${req.body.vin} is missing`})
+  } else if(!req.body.make){
+    res.status(400).json({message: `${req.body.make} is missing`})
+  } else if(!req.body.model){
+    res.status(400).json({message: `${req.body.model} is missing`})
+  } else if(!req.body.mileage){
+    res.status(400).json({message: `${req.body.mileage} is missing`})
+  } else {
+    req.body.vin = req.body.vin.trim()
+    req.body.make = req.body.make.trim()
+    req.body.model = req.body.model.trim()
+    req.body.mileage = req.body.mileage.trim()
+    next()
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  try{
+    const vin = await Car.getAll()
+  
+  } 
+  catch(err){
+    next(err)
+  }
+}
+
+module.exports = {
+  checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid
 }
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,10 @@
-// STRETCH
+exports.seed = function(knex){
+    return knex("cars").truncate()
+        .then(function () {
+            return knex("cars").insert([
+                {vin: "12344", make: "Dodge", model: "ram", mileage:35, title: "Big truck", transmission: "12"},
+                {vin: "111111", make: "Ford", model: "mustang", mileage:35, title: "sports car"},
+                {vin: "123222244", make: "Jeep", model: "wranger", mileage:35, transmission: "12"}
+            ])
+        })
+}
