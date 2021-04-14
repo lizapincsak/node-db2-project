a6a57c8152e6c3bd991711837c145be6b86ce29e
Liza Pincsak <lizapincsak@MacBook-Air-2.local>
2021-04-13 20:42:12 -0500

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,4 +1,5 @@
 const Car = require("./cars-model");
+const vinValidator = require("vin-validator");
 
 const checkCarId = async(req, res, next) => {
   try{
@@ -17,30 +18,46 @@ const checkCarId = async(req, res, next) => {
 
 const checkCarPayload = (req, res, next) => {
   if(!req.body.vin){
-    res.status(400).json({message: `${req.body.vin} is missing`})
+    res.status(400).json({message: `vin is missing`})
   } else if(!req.body.make){
-    res.status(400).json({message: `${req.body.make} is missing`})
+    res.status(400).json({message: `make is missing`})
   } else if(!req.body.model){
-    res.status(400).json({message: `${req.body.model} is missing`})
+    res.status(400).json({message: `model is missing`})
   } else if(!req.body.mileage){
-    res.status(400).json({message: `${req.body.mileage} is missing`})
+    res.status(400).json({message: `mileage is missing`})
   } else {
-    req.body.vin = req.body.vin.trim()
-    req.body.make = req.body.make.trim()
-    req.body.model = req.body.model.trim()
-    req.body.mileage = req.body.mileage.trim()
     next()
   }
 }
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberValid = async (req, res, next) => {
+  try{
+    const isValidVin = vinValidator.validate(req.body.vin)
+    if(!isValidVin){
+      res.status(400).json({message: `vin ${req.body.vin} is invalid`})
+    } else {
+      next()
+    }
+  }
+  catch(err){
+    next(err)
+  }
 }
 
 const checkVinNumberUnique = async (req, res, next) => {
   try{
-    const vin = await Car.getAll()
-  
+    const allCars = await Car.getAll()
+    const vin = req.body.vin
+    const results = allCars.filter(item => {
+      if(item.vin === vin){
+        return item
+      }
+    })
+    if(results.length > 0){
+      res.status(400).json({message: `vin ${req.body.vin} already exists`})
+    } else {
+      next()
+    }
   } 
   catch(err){
     next(err)
